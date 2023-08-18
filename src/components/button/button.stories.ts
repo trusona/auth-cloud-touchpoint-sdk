fc66d0dba4b6169a96e2f02e06a0ed3d04ef5493
Kevin Villalobos <kevin.villalobos@trusona.com>
2023-08-18 15:36:05 -0600

    Start Touchpoint
    - Create Start Touchpoint Lit component
    - Add Story and Static HTML example
    - Code Refactor
    - Fix some storybook arguments

--- src/components/button/button.stories.ts
+++ src/components/button/button.stories.ts
@@ -1,91 +1,76 @@
-import { Meta, StoryObj } from '@storybook/web-components'
-import { html } from 'lit'
+import {Meta, StoryObj} from '@storybook/web-components'
+import {html} from 'lit'
 
 import './button'
 
 export default {
-  title: 'Auth Button',
-  parameters: {
-    layout: 'centered',
-  },
-  argTypes: {
-    onClick: { description: 'Callback when button is clicked' },
-    isProcessing: { description: 'Add this attribute when user has clicked the button and action is being executed.' },
-    isWaitingForInput: { description: 'Add this attribute when user has not entered in a username or completed a valid email.'},
-  }
+    title: 'Auth Button',
+    parameters: {
+        layout: 'centered',
+    },
+    argTypes: {
+        onClick: {description: 'Callback when button is clicked'},
+        isProcessing: {description: 'Add this attribute when user has clicked the button and action is being executed.'},
+        isWaitingForInput: {description: 'Add this attribute when user has not entered in a username or completed a valid email.'},
+    }
 } as Meta
 
 export const Enabled: StoryObj = {
-  name: 'Enabled',
-  args: {
-    isProcessing: false,
-    isWaitingForInput: false,
-    onClick: () => {
-      alert('Button Clicked!')
+    name: 'Enabled',
+    args: {
+        isProcessing: false,
+        isWaitingForInput: false,
+        onClick: () => {
+            alert('Button Clicked!')
+        }
+    },
+    render: (args) => {
+        return html`
+            <auth-button
+                    ?isProcessing="${args.isProcessing}"
+                    ?isWaitingForInput="${args.isWaitingForInput}"
+                    @onClick=${args.onClick}>
+                Create Account
+            </auth-button>`
     }
-  },
-  render: (args) => {
-    args.isProcessing = false
-    args.isWaitingForInput = false
-    args.onClick = () => {
-      alert('Button Clicked!')
-    }
-
-    return html`
-      <auth-button
-          @onClick=${args.onClick}>
-        Create Account
-      </auth-button>`
-  }
 }
 
 export const WaitingForInput: StoryObj = {
-  name: 'Waiting for input',
-  args: {
-    isProcessing: false,
-    isWaitingForInput: true,
-    onClick: () => {
-      alert('Button Clicked!')
-    }
-  },
-  render: (args) => {
-    args.isProcessing = false
-    args.isWaitingForInput = true
-    args.onClick = () => {
-      alert('Button Clicked!')
-    }
+    name: 'Waiting for input',
+    args: {
+        isProcessing: false,
+        isWaitingForInput: true,
+        onClick: () => {
+            alert('Button Clicked!')
+        }
+    },
+    render: (args) => {
 
-    return html`
-      <auth-button
-          isWaitingForInput="${args.isWaitingForInput}"
-          @onClick=${args.onClick}>
-        Create Account
-      </auth-button>`
-  }
+        return html`
+            <auth-button
+                    ?isWaitingForInput="${args.isWaitingForInput}"
+                    @onClick=${args.onClick}>
+                Create Account
+            </auth-button>`
+    }
 }
 
 export const Processing: StoryObj = {
-  name: 'Processing',
-  args: {
-    isProcessing: true,
-    isWaitingForInput: false,
-    onClick: () => {
-      alert('Button Clicked!')
+    name: 'Processing',
+    args: {
+        isProcessing: true,
+        isWaitingForInput: false,
+        onClick: () => {
+            alert('Button Clicked!')
+        }
+    },
+    render: (args) => {
+        return html`
+            <auth-button
+                    ?isProcessing="${args.isProcessing}"
+                    @onClick=${args.onClick}>
+                Create Account
+            </auth-button>`
     }
-  },
-  render: (args) => {
-    args.isProcessing = true
-    args.isWaitingForInput = true
-    args.onClick = () => {
-      alert('Button Clicked!')
-    }
-
-    return html`
-      <auth-button
-          isProcessing="${args.isProcessing}"
-          @onClick=${args.onClick}>
-        Create Account
-      </auth-button>`
-  }
 }
 
---
+++ src/login/start/start-touchpoint.stories.ts
@@ -0,0 +1,142 @@
+import {Meta, StoryObj} from '@storybook/web-components'
+import {html} from 'lit'
+
+import './start-touchpoint'
+
+export default {
+    title: 'Start Touchpoint',
+    parameters: {
+        layout: 'centered',
+    },
+    argTypes: {
+        started: {
+            description: 'Property to indicate if the Flow has been Start, is TRUE this will set the button to the processing state',
+            table: {defaultValue: {summary: 'false'}}
+        },
+        waitingForInput: {
+            description: 'Property for Button to be enabled',
+            table: {defaultValue: {summary: 'true'}}
+        },
+        isEmailInput: {
+            description: 'Component Property to indicate if the Input is an Email',
+            table: {defaultValue: {summary: 'false'}}
+        },
+        inputAutofocus: {
+            description: 'Set the Input Element the Autofocus property',
+            table: {defaultValue: {summary: 'false'}}
+        },
+        inputRequired: {
+            description: 'Set the Input Element the Required property',
+            table: {defaultValue: {summary: 'false'}}
+        },
+        inputDisabled: {
+            description: 'Set the Input Element the Disabled property',
+            table: {defaultValue: {summary: 'false'}}
+        },
+        inputId: {
+            description: 'Input Element Id',
+            table: {defaultValue: {summary: 'username'}}
+        },
+        inputName: {
+            description: 'Input Element Name',
+            table: {defaultValue: {summary: 'username'}}
+        },
+        inputType: {
+            description: 'Input Element Type',
+            table: {defaultValue: {summary: 'username'}}
+        },
+        inputAutocomplete: {
+            description: 'Auto Complete Tags',
+            table: {defaultValue: {summary: 'username webauthn'}}
+        },
+        login: {
+            description: 'Login Model Object',
+            table: {type: 'Object'}
+        },
+        globalStyles: {
+            description: 'Global Styles as custom CSS styling for components',
+            table: {type: 'Object'}
+        },
+        signInWithBiometricPrompt: {
+            description: 'Sign In with Biometric Object',
+            table: {type: 'Object'}
+        },
+        signInAnotherWay: {
+            description: 'Callback when the Sign In Another Way Button is clicked',
+            table: {type: 'Function'}
+        },
+        performContinue: {
+            description: 'Callback when the Continue button is clicked.',
+            table: {type: 'Function'}
+        },
+    }
+} as Meta
+
+export const Default: StoryObj = {
+    name: 'Default',
+    args: {
+        inputDisabled: false,
+        inputRequired: true,
+        isEmailInput: false,
+        started: false,
+        waitingForInput: false,
+        inputAutofocus:true,
+        inputId: 'username',
+        inputName: 'username',
+        inputType: 'username',
+        inputAutocomplete: 'username webauthn',
+        login: {},
+        globalStyles: {},
+        signInWithBiometricPrompt: {},
+        performContinue: () => {
+            alert('Button Clicked!')
+        }
+    },
+    render: (args) => {
+        return html`
+            <start-touchpoint inputId="${args.inputId}"
+                              inputName="${args.inputName}"
+                              inputType="${args.inputType}"
+                              inputAutocomplete="${args.inputAutocomplete}"
+                              ?started="${args.started}"
+                              ?waitingForInput="${args.waitingForInput}"
+                              isEmailInput="${args.isEmailInput}"
+                              inputRequired="${args.inputRequired}"
+                              inputDisabled="${args.inputDisabled}"
+                              inputAutofocus="${args.inputAutofocus}"
+                              performContinue="${() => args.performContinue()}"
+                              login="${args.login}"
+                              globalStyles="${args.globalStyles}"
+                              signInWithBiometricPrompt="${args.signInWithBiometricPrompt}"
+            />
+        `
+    }
+}
+export const Email: StoryObj = {
+    name: 'Email',
+    render: () => {
+        return html`
+            <start-touchpoint inputId="email_touchpoint"
+                              inputName="email_touchpoint"
+                              inputType="email"
+                              isEmailInput="true"
+                              inputRequired="true"
+                              inputDisabled="false"
+                              performContinue="${() => alert('Button Clicked!')}"/>
+        `
+    }
+}
+
+export const Username: StoryObj = {
+    name: 'Username',
+    render: () => {
+        return html`
+            <start-touchpoint inputId="username_touchpoint"
+                              inputName="username_touchpoint"
+                              signInWithBiometricPrompt='{ "usernameFieldHint" : "Username", "usernameControlText" : "Username" }'
+                              inputRequired="true"
+                              inputDisabled="false"
+                              performContinue="${() => alert('Button Clicked!')}"/>
+        `
+    }
+}
