fd266cacc225b2b4b992380c1052bde15a9ab7f6
Kevin Villalobos <kevin.villalobos@trusona.com>
2023-09-21 11:48:27 -0600

    Fido Touchpoints
    - Update static page for Fido Passkey Details
    - Update Storybooks for  Fido Passkey Details and Card

--- src/components/fido-passkey-details-card/fido-passkey-details-card.stories.ts
+++ src/components/fido-passkey-details-card/fido-passkey-details-card.stories.ts
@@ -9,18 +9,44 @@ export default {
         layout: 'centered',
     },
     argTypes: {
-
+        savedText: {
+            description: "Text to indicate the Created Date"
+        },
+        lastUsedText: {
+            description: "Text to indicate the Last Used Date"
+        },
+        lastUsedIsMobile: {
+            description: "Boolean to indicate if the Last Usage was on Mobile",
+            table: {defaultValue: {summary: 'false'}}
+        },
+        prevLastUsedText: {
+            description: "[Optional] Text to indicate the Previous Last Used Date"
+        },
+        prevLastUsedIsMobile: {
+            description: "[Optional] Boolean to indicate if the Previous Last Usage was on Mobile",
+            table: {defaultValue: {summary: 'false'}}
+        }
     }
 } as Meta
 
 export const Default: StoryObj = {
     name: 'Default',
     args:{
-
+        savedText: "Saved with iOS 16.2 on April 11, 2023, 12:01am",
+        lastUsedText:"iOS 16.2, April 11, 2023, 3:42pm",
+        lastUsedIsMobile: true,
+        prevLastUsedText:"MacOS 13.0.1, April 23rd, 2023, 4:21pm",
+        prevLastUsedIsMobile:false
     },
     render: (args) => {
         return html`
-            <fido-passkey-details-card>
+            <fido-passkey-details-card
+                    savedText="${args.savedText}"
+                    lastUsedText="${args.lastUsedText}"
+                    lastUsedIsMobile="${args.lastUsedIsMobile}"
+                    prevLastUsedText="${args.prevLastUsedText}"
+                    prevLastUsedIsMobile="${args.prevLastUsedIsMobile}"
+            >
             </fido-passkey-details-card>
         `
     }
--- src/touchpoints/fido-passkey-details/fido-passkey-details.stories.ts
+++ src/touchpoints/fido-passkey-details/fido-passkey-details.stories.ts
@@ -10,28 +10,42 @@ export default {
         layout: 'centered',
     },
     argTypes: {
-
+        passkeyDetails: {
+            description: `Array of PasskeyDetails Objects.
+            
+                PasskeyDetails {
+                    savedText: string,
+                    lastUsedText: string
+                    lastUsedIsMobile: boolean
+                    prevLastUsedText?: string | null
+                    prevLastUsedIsMobile?: boolean | null
+                }`,
+            control: {
+                type: 'object'
+            }
+        },
     }
 } as Meta
 
+let passkeyDetails : PasskeyDetails[] = [
+    {
+        savedText: "Saved with iOS 16.2 on April 11, 2023, 12:01am",
+        lastUsedText:"iOS 16.2, April 11, 2023, 3:42pm",
+        lastUsedIsMobile: true,
+        prevLastUsedText:"MacOS 13.0.1, April 23rd, 2023, 4:21pm",
+        prevLastUsedIsMobile:false
+    },
+    {
+        savedText: "Saved with Android 13 on April 11, 2023, 12:01am",
+        lastUsedText:"Android 13, April 11th, 2023, 3:42pm",
+        lastUsedIsMobile: true
+    },
+]
 
 export const Default: StoryObj = {
     name: 'Default',
     args:{
-        passkeyDetails : [
-            {
-                savedText: "Saved with iOS 16.2 on April 11, 2023, 12:01am",
-                lastUsedText:"iOS 16.2, April 11, 2023, 3:42pm",
-                lastUsedIsMobile: true,
-                prevLastUsedText:"MacOS 13.0.1, April 23rd, 2023, 4:21pm",
-                prevLastUsedIsMobile:false
-            },
-            {
-                savedText: "Saved with Android 13 on April 11, 2023, 12:01am",
-                lastUsedText:"Android 13, April 11th, 2023, 3:42pm",
-                lastUsedIsMobile: true
-            },
-        ]
+        passkeyDetails : passkeyDetails
     },
     render: (args) => {
         return html`
